8898f7d14f14e08be6b41b1a2673f0f425cae17c
Michal Kreft <yomajkel@gmail.com>
2016-09-04 16:23:08 +0100

    Initial Commit

---
+++ JLDishwashersTests/JLDishwashersTests.swift
@@ -0,0 +1,36 @@
+//
+//  JLDishwashersTests.swift
+//  JLDishwashersTests
+//
+//  Created by Michal Kreft on 04/09/2016.
+//  Copyright © 2016 erigo software Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import JLDishwashers
+
+class JLDishwashersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
